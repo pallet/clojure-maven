e8c9df722502f4a3db877289adfc48c7c029fbef
Hugo Duncan <hugo@hugoduncan.org>
2012-03-13 12:31:17 -0400

    Fix defmojo-test failures

--- mojo/src/test/clojure/clojure/maven/mojo/defmojo_test.clj
+++ mojo/src/test/clojure/clojure/maven/mojo/defmojo_test.clj
@@ -7,19 +7,12 @@
    [clojure.string :as str]
    [clojure.maven.mojo.log :as log])
   (:import
-   org.apache.maven.plugin.ContextEnabled
-   org.apache.maven.plugin.Mojo
-   org.apache.maven.plugin.MojoExecutionException))
-
-
+   [org.apache.maven.plugin ContextEnabled Mojo MojoExecutionException]))
 
 (deftest defmacro-minimal
-
   (is (=
        (macroexpand-1
-        '(defmojo
-             MyFirstMojo {} [] (log/info "Hello World!")))
-
+        `(defmojo ~'MyFirstMojo {} [] (log/info "Hello World!")))
        '(do
           (clojure.core/deftype MyFirstMojo
             [ ^{:volatile-mutable true} log
@@ -29,7 +22,7 @@
             (setLog [_ logger] (set! log logger))
             (getLog [_] log)
             (execute [this] (clojure.maven.mojo.log/with-log log
-                              (log/info "Hello World!")))
+                              (clojure.maven.mojo.log/info "Hello World!")))
 
             org.apache.maven.plugin.ContextEnabled
             (setPluginContext [_ context] (reset! plugin-context context))
@@ -41,17 +34,16 @@
 
 
 (deftest defmacro-basic
-
   ;; Note: By definition '=' doesn't compare metadata.
   ;; Since defmojo generates quite a bit of metadata, we are not really testing
   ;; all there is to it here :-(
   (is (=
        (macroexpand-1
-        '(defmojo
-             MyFirstMojo {:goal "hello-world"}
+        `(defmojo
+             ~'MyFirstMojo {:goal "hello-world"}
              [
-              project {:expression "${project}"}
-              basedir {:expression "${basedir}"}
+              ~'project {:expression "${project}"}
+              ~'basedir {:expression "${basedir}"}
               ]
            (log/info "Hello World!")))
 
@@ -71,7 +63,7 @@
             (getLog [_] log)
 
             (execute [this] (clojure.maven.mojo.log/with-log log
-                              (log/info "Hello World!")))
+                              (clojure.maven.mojo.log/info "Hello World!")))
 
             org.apache.maven.plugin.ContextEnabled
             (setPluginContext [_ context] (reset! plugin-context context))
@@ -85,59 +77,59 @@
 
 (deftest body-missing
   (is (thrown-with-msg? IllegalArgumentException #"Body.*missing"
-        (macroexpand-1 '(defmojo Test {} [])))))
+        (macroexpand-1 `(defmojo ~'Test {} [])))))
 
 (deftest body-multiple-sexps
   (is (thrown-with-msg? IllegalArgumentException #"Body.*do"
-        (macroexpand-1 '(defmojo Test {} []
+        (macroexpand-1 `(defmojo ~'Test {} []
                           (log/info "first")
                           (log/info "second"))))))
 
 (deftest invalid-mojo-annotations
   (is (thrown-with-msg? IllegalArgumentException #"annotation"
-        (macroexpand-1 '(defmojo Test
-                            non-map
+        (macroexpand-1 `(defmojo ~'Test
+                            ~'non-map
                             []
                           (log/info "hello world!"))))))
 
 
 (deftest invalid-mojo-annotation
-  (is (thrown-with-msg? IllegalArgumentException #"annotation"
-        (macroexpand-1 '(defmojo Test
+  (is (thrown-with-msg? Exception #"annotation"
+        (macroexpand-1 `(defmojo ~'Test
                             {:goalie "test"}
                             []
                           (log/info "hello world!"))))))
 
 (deftest invalid-params-vector
   (is (thrown-with-msg? IllegalArgumentException #"vector.*param"
-        (macroexpand-1 '(defmojo
-                            Test {}
-                            non-vector
+        (macroexpand-1 `(defmojo
+                            ~'Test {}
+                            ~'non-vector
                           (log/info "hello world!"))))))
 
 (deftest invalid-param-pair
   (is (thrown-with-msg? IllegalArgumentException #"parameter"
-        (macroexpand-1 '(defmojo
-                            Test {}
-                            [param-name]
+        (macroexpand-1 `(defmojo
+                            ~'Test {}
+                            [~'param-name]
                           (log/info "hello world!")))))
 
   (is (thrown-with-msg? IllegalArgumentException #"parameter"
-        (macroexpand-1 '(defmojo
-                            Test {}
-                            [param-name [:required "true"]]
+        (macroexpand-1 `(defmojo
+                            ~'Test {}
+                            [~'param-name [:required "true"]]
                           (log/info "hello world!")))))
 
   (is (thrown-with-msg? IllegalArgumentException #"parameter"
-        (macroexpand-1 '(defmojo
-                            Test {}
-                            [param-name no-map]
+        (macroexpand-1 `(defmojo
+                            ~'Test {}
+                            [~'param-name ~'no-map]
                           (log/info "hello world!"))))))
 
 (deftest invalid-param-pair
-  (is (thrown-with-msg? IllegalArgumentException #"parameter"
-        (macroexpand-1 '(defmojo
-                            Test
+  (is (thrown-with-msg? Exception #"parameter"
+        (macroexpand-1 `(defmojo
+                            ~'Test
                             {:goal "test"}
-                            [param-name no-map]
+                            [~'param-name ~'no-map]
                           (log/info "hello world!"))))))
